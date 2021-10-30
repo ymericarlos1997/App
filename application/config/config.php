f4fd2f9cb504c11bb43ccdbe74832bccf8a98bab
ifredyorlando <tooheyred@gmail.com>
2021-10-30 09:43:59 -0600

    Cambios

--- application/config/config.php
+++ application/config/config.php
@@ -21,7 +21,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 | If you need to allow multiple domains, remember that this file is still
 | a PHP script and you can easily do that on your own.
-|
+|assets/template/dist/img/logo21.PNG
 */
 $config['base_url'] = '';
 
--- application/models/Usuarios_model.php
+++ application/models/Usuarios_model.php
@@ -37,7 +37,7 @@ class Usuarios_model extends CI_Model {
 		$this->db->where("username", $username);
 		$this->db->where("password", $password);
 
-		$resultados = $this->db->get("usuarios");
+		$resultados = $this->db->get("Cliente");
 		if ($resultados->num_rows() > 0) {
 			return $resultados->row();
 		}
